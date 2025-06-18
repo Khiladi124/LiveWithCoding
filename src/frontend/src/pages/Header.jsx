28af916e959ede0dccf6db5876c078a9d1d4769d
Abhishek-Gangwar-0401 <abhishek.gangwar.04.001@gmail.com>
2025-06-18 07:42:10 +0530

    frontend issues resolved

--- src/frontend/src/pages/Header.jsx
+++ src/frontend/src/pages/Header.jsx
@@ -187,13 +187,13 @@ const Header = () => {
                         >
                             About
                         </button>
-                        <button
+                        {/* <button
                             onClick={() => navigate('/linkedinBot')}
                             className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors relative z-50 shadow-md"
                             >
                                 Ask Referral
                             
-                        </button>
+                        </button> */}
                     </div>
                 </div>
             </header>
