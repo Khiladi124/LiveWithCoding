15c2ed54088686b2a980b622170a1eb281fb4185
Abhishek-Gangwar-0401 <abhishek.gangwar.04.001@gmail.com>
2025-06-20 17:19:56 +0530

    some updates

--- src/backend/middlewares/auth.middleware.js
+++ src/backend/middlewares/auth.middleware.js
@@ -6,6 +6,7 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async (req, res, next) => {
 
     // Get access token from cookies or header (Header Format-> "Authorization: Bearer <token>")
+    console.log("Request cookies:", req.cookies); // DEBUGGING
     const token =
         req.cookies?.accessToken ||
         req.header("Authorization")?.replace("Bearer ", ""); // Separating token from header
