994710814d133cc3f6f779cd41511fd96b471edd
Abhishek-Gangwar-0401 <abhishek.gangwar.04.001@gmail.com>
2025-07-06 14:11:06 +0530

    final adjust

--- src/backend/utils/SendMail.js
+++ src/backend/utils/SendMail.js
@@ -2,6 +2,7 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async (to, url, otp, txt) => {
    const message= otp?"Email Verification":"Reset Password";
+   var _otp=otp?otp:"";
   try {
     console.log(process.env.SENDER_EMAIL_ADDRESS, process.env.GMAIL_APP_PASSWORD); // DEBUGGING
     const transporter = nodemailer.createTransport({
@@ -21,7 +22,7 @@ const sendEmail = async (to, url, otp, txt) => {
           <h2 style="text-align: center; text-transform: uppercase;color: #4d23af;">Welcome to JioCoding.</h2>
          <p>Congratulations! You're almost set to start using <b>JioCoding</b>.</p>
           <p> Just click the button below to ${txt}.</p>
-          ${otp&&<h1 style="text-align: center; color: blue;"> ${otp}</h1>}
+          <h1 style="text-align: center; color: blue;"> ${otp}</h1>
           <a href="${url}" style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: auto; display: inline-block;">${txt}</a>
           <p>If the button doesn't work, you can also use this link:</p>
           <div>${url}</div>
