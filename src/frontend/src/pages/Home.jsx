634589a316ae1f63c8053bd34c603749290fdc2e
Abhishek-Gangwar-0401 <abhishek.gangwar.04.001@gmail.com>
2025-07-06 11:49:49 +0530

    bug resolved home

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -225,7 +225,7 @@ const Home = () => {
                                             <td className="px-8 py-6">
                                                 <div className="w-6 h-6 rounded-full border-2 border-emerald-300 group-hover:border-emerald-500 transition-colors duration-200 flex items-center justify-center">
                                                     {
-                                                    user && user.problemSolved && user.problemSolved.includes(problem.problemId) ?
+                                                    user && user.problemSolved && user.problemSolved.some(id => id === problem.problemId) ?
                                                     (
                                                         <svg className="w-4 h-4 text-emerald-500" fill="currentColor" viewBox="0 0 20 20">
                                                             <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
